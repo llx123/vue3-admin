d1c4e4243399f9ce691aa6ac78c23f7ab07f83f5
lxli <573381010@qq.com>
2023-02-07 01:09:49 +0800

    swiper

--- src/views/discover/Banner.vue
+++ src/views/discover/Banner.vue
@@ -1,17 +1,26 @@
 <template>
-  <swiper slides-per-view="auto" :grap-cursor="true">
-    <SwiperSlide>111</SwiperSlide>
-    <SwiperSlide>222</SwiperSlide>
-    <SwiperSlide>333</SwiperSlide>
-    <SwiperSlide>444</SwiperSlide>
-    <SwiperSlide>555</SwiperSlide>
-    <SwiperSlide>666</SwiperSlide>
-  </swiper>
+  <div class="banner-swiper">
+    <swiper :slides-per-view="num" :grap-cursor="true">
+      <SwiperSlide>111</SwiperSlide>
+      <SwiperSlide>222</SwiperSlide>
+      <SwiperSlide>333</SwiperSlide>
+      <SwiperSlide>444</SwiperSlide>
+      <SwiperSlide>555</SwiperSlide>
+      <SwiperSlide>666</SwiperSlide>
+    </swiper>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { Swiper, SwiperSlide } from "swiper/vue";
 import "swiper/css";
+
+const num = 4
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.banner-swiper {
+  width: 80vw;
+  margin: 0 auto;
+}
+</style>
